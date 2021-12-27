85c7fcaedd15249abdee03aad5d6942c414f2961
w11knd <1968227046@qq.com>
2021-12-27 13:03:51 +0800

    [Refactor] Remove unsigned int & double support

--- include/Common/Core.hpp
+++ include/Common/Core.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <type_traits>
+
 #ifdef _MSC_VER
-    #define Euclid_Forceinline [[msvc::forceinline]]
+    #define EUCLID_FORCEINLINE [[msvc::forceinline]]
+/* 
 #elif __has_cpp_attribute(gnu::always_inline)
-    #define Euclid_Force_Inline [[gnu::always_inline]]
+    #define Euclid_Forceinline [[gnu::always_inline]]
+*/
 #else
-    #define Euclid_Force_Inline inline __attribute__((always_inline))
+    #define Euclid_Forceinline inline __attribute__((always_inline))
 #endif // Euclid_Forceinline
 
 namespace euclid {
@@ -19,10 +23,10 @@ template<typename Ty, typename... Rest>
 inline constexpr bool is_any_type_of_v = std::disjunction_v<std::is_same<Ty, Rest>...>;
 
 template<typename Ty>
-inline constexpr bool is_arithmetic_type_v = is_any_type_of_v<Ty, int, unsigned int, float, double>;
+inline constexpr bool is_arithmetic_type_v = is_any_type_of_v<Ty, int, float>;
 
 template<typename Ty>
-inline constexpr bool is_float_point_type_v = is_any_type_of_v<Ty, float, double>;
+inline constexpr bool is_float_point_type_v = is_any_type_of_v<Ty, float>;
 
 }
 
@@ -44,16 +48,12 @@ struct arithmetic_promotion {
 template<arithmetic... Ty>
 using arithmetic_promotion_t = typename arithmetic_promotion<Ty...>::type;
 
-template<typename Ty>
-concept signed_type = detail::is_any_type_of_v<Ty, int, float, double>;
+template<typename Des, typename Src>
+concept acceptable_loss = float_point_type<Des> || same_type<Des, Src>;
 
 template<typename Des, typename Src>
-concept acceptable_loss = signed_type<Des> && same_type<Des, arithmetic_promotion_t<Des, Src>>;
-
-template<typename Ty, typename CastT>
-concept promotion_cast = acceptable_loss<CastT, Ty> && !same_type<Ty, CastT>;
-
-template<typename Des, typename Src>
-concept acceptable_loss_basic = float_point_type<Des> || same_type<Des, Src>;
+concept bitwise_copyable = (sizeof(Des) == sizeof(Src)) && 
+                           std::is_trivially_copyable_v<Des> && 
+                           std::is_trivially_copyable_v<Src>;
 
 }
