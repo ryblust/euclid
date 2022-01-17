975d719fb74541354c6a5fdae9b1c89142f2975c
w11knd <1968227046@qq.com>
2022-01-18 05:19:34 +0800

    Add type traits support

--- include/Matrix3.hpp
+++ include/Matrix3.hpp
@@ -14,4 +14,14 @@ Mat3(First, Rest...)->Mat3<First>;
 using mat3i = Mat3<int>;
 using mat3f = Mat3<float>;
 
+template<typename T>
+struct is_mat3 {
+    static constexpr bool value = false;
+};
+
+template<euclid_type T>
+struct is_mat3<Mat3<T>> {
+    static constexpr bool value = true;
+};
+
 }
