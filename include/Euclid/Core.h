91ab45a850941e88a58be8d33c9fd7ba64267b68
w11knd <1968227046@qq.com>
2022-02-02 23:41:55 +0800

    Complete Concept Support

--- include/Euclid/Core.h
+++ include/Euclid/Core.h
@@ -16,8 +16,8 @@
 // MSVC has better constexpr support for simd intrinsics
 #define EUCLID_FUNC_QUALIFIER EUCLID_FORCEINLINE EUCLID_MSVC_CONSTEXPR
 
-#if defined(_MSC_VER) && !defined(EUCLID_NO_OPERATOR_OVERLOAD) 
-    #define EUCLID_HAS_OPERATOR_OVERLOAD
+#if defined(_MSC_VER) && !defined(EUCLID_NO_GLOBAL_OPERATOR_OVERLOAD) 
+    #define EUCLID_HAS_GLOBAL_OPERATOR_OVERLOAD
 #endif
 
 namespace euclid {
@@ -25,12 +25,25 @@ namespace euclid {
 using vec4i = __m128i;
 using vec4f = __m128;
 using vec4d = __m256d;
-
 using vec8i = __m256i;
 using vec8f = __m256;
 
+struct vec2i;
+struct vec2f;
+struct vec3i;
+struct vec3f;
+struct mat2i;
+struct mat2f;
+struct mat3i;
+struct mat3f;
+struct mat4i;
+struct mat4f;
+
 template<typename Ty1, typename Ty2>
 concept same_type = std::is_same_v<Ty1, Ty2>;
+
+template<typename Ty>
+concept arithmetic = std::is_arithmetic_v<Ty>;
 
 template<typename Ty>
 concept floating_point = std::is_floating_point_v<Ty>;
@@ -38,8 +51,7 @@ concept floating_point = std::is_floating_point_v<Ty>;
 template<typename First, typename... Rest>
 concept is_any_type_of = std::disjunction_v<std::is_same<First, Rest>...>;
 
-template<typename Ty>
-concept euclid_type = is_any_type_of<Ty, int, float>;
+namespace trait {
 
 #ifdef __GNUC__
 #pragma GCC diagnostic push
@@ -47,10 +59,19 @@ concept euclid_type = is_any_type_of<Ty, int, float>;
 #endif
 
 template<typename T>
-concept vector_type = is_any_type_of<T, vec4i, vec4f, vec4d, vec8i, vec8f>;
+concept vector_type = is_any_type_of<T, vec2i, vec2f, vec3i, vec3f,                    
+                                        vec4i, vec4f, vec4d, vec8i, vec8f>;
+
+template<typename T>
+concept matrix_type = is_any_type_of<T, mat2i, mat2f, mat3i, mat3f, mat4i, mat4f>;
+
+template<typename T>
+concept euclid_component = vector_type<T> || matrix_type<T>;
 
 #ifdef __GNUC__
 #pragma GCC diagnostic pop
 #endif
+
+}
 
 }
