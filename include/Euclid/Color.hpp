3aea048bcaa9c1a51ce3b47f5aa64be2bfec6c55
w11knd <1968227046@qq.com>
2022-02-22 21:41:32 +0800

    [Color] Remove alignas

--- include/Euclid/Color.hpp
+++ include/Euclid/Color.hpp
@@ -2,18 +2,21 @@
 
 #include "Core.h"
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4201)
+// C4201 : Use the non-standard extension : unnamed union
+#endif
+
 namespace euclid {
 
 enum class GammaSpace : std::uint8_t { Linear, sRGB };
 
-struct alignas(16) LinearColor {
-    
+struct LinearColor {
     float r,g,b,a;
 };
 
-struct alignas(8) Color {
-
-
+struct Color {
     union {
         struct {
             std::uint8_t r,g,b,a;
@@ -23,3 +26,7 @@ struct alignas(8) Color {
 };
 
 }
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
