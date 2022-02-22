402f9b73f1d34ca0232c2dc93f4e98462fd90be6
w11knd <1968227046@qq.com>
2022-02-22 21:44:50 +0800

    Add include files

--- include/Euclid/Euclid.h
+++ include/Euclid/Euclid.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "Math.hpp"
-#include "Vec3.hpp"
 #include "Mat2.hpp"
+#include "Mat3.hpp"
 #include "Mat4.hpp"
+#include "Geometric.hpp"
 #include "Color.hpp"
 #include "Utility.hpp"
