bea0b840c8d76b6bc317d82b63484bc750353126
zhuziming <73880214+w11knd@users.noreply.github.com>
2022-03-19 20:50:22 +0800

    2022.3.19

--- include/Euclid/Geometric.hpp
+++ include/Euclid/Geometric.hpp
@@ -14,30 +14,46 @@
 
 namespace euclid {
 
-EUCLID_QUALIFIER float EUCLID_CALL dot(const Vec2 a, const Vec2 b) noexcept {
+constexpr float EUCLID_CALL dot(const Vec2 a, const Vec2 b) noexcept {
     return a.x * b.x + a.y * b.y;
 }
 
-EUCLID_QUALIFIER float EUCLID_CALL dot(const Vec3 a, const Vec3 b) noexcept {
+constexpr float EUCLID_CALL dot(const Vec3 a, const Vec3 b) noexcept {
     return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+constexpr float EUCLID_CALL length(const Vec2 a) noexcept {
+    return math::sqrt(dot(a, a));
+}
+
+constexpr float EUCLID_CALL length(const Vec3 a) noexcept {
+    return math::sqrt(dot(a, a));
+}
+
+constexpr Vec2 EUCLID_CALL normalize(const Vec2 a) noexcept {
+    return a / length(a);
+}
+
+constexpr Vec3 EUCLID_CALL normalize(const Vec3 a) noexcept {
+    return a / length(a);
+}
+
+constexpr Vec3 EUCLID_CALL cross(const Vec3 a, const Vec3 b) noexcept {
+    return {
+        a.y * b.z - a.z * b.y,
+        a.z * b.x - a.x * b.z,
+        a.x * b.y - a.y * b.x
+    };
 }
 
 EUCLID_QUALIFIER float EUCLID_CALL dot(const Vec4 a, const Vec4 b) noexcept {
     if (__builtin_is_constant_evaluated()) {
         return getVec4Data(a, 0) * getVec4Data(b, 0) +
-               getVec4Data(a, 1) * getVec4Data(b, 1) +
-               getVec4Data(a, 2) * getVec4Data(b, 2) +
-               getVec4Data(a, 3) * getVec4Data(b, 3);
+            getVec4Data(a, 1) * getVec4Data(b, 1) +
+            getVec4Data(a, 2) * getVec4Data(b, 2) +
+            getVec4Data(a, 3) * getVec4Data(b, 3);
     }
     return _mm_cvtss_f32(_mm_dp_ps(a, b, 0xff));
-}
-
-EUCLID_QUALIFIER float EUCLID_CALL length(const Vec2 a) noexcept {
-    return math::sqrt(dot(a, a));
-}
-
-EUCLID_QUALIFIER float EUCLID_CALL length(const Vec3 a) noexcept {
-    return math::sqrt(dot(a, a));
 }
 
 EUCLID_QUALIFIER float EUCLID_CALL length(const Vec4 a) noexcept {
@@ -56,14 +72,6 @@ EUCLID_QUALIFIER float EUCLID_CALL lengthEst(const Vec4 a) noexcept {
     return _mm_cvtss_f32(_mm_mul_ps(rsqrt, dpvec));
 }
 
-EUCLID_QUALIFIER Vec2 EUCLID_CALL normalize(const Vec2 a) noexcept {
-    return a / length(a);
-}
-
-EUCLID_QUALIFIER Vec3 EUCLID_CALL normalize(const Vec3 a) noexcept {
-    return a / length(a);
-}
-
 EUCLID_QUALIFIER Vec4 EUCLID_CALL normalize(const Vec4 a) noexcept {
     if (__builtin_is_constant_evaluated()) {
         return a / length(a);
@@ -76,14 +84,6 @@ EUCLID_QUALIFIER Vec4 EUCLID_CALL normalizeEst(const Vec4 a) noexcept {
         return a / math::sqrt(dot(a, a));
     }
     return _mm_mul_ps(a, _mm_rsqrt_ps(_mm_dp_ps(a, a, 0xff)));
-}
-
-EUCLID_QUALIFIER Vec3 EUCLID_CALL cross(const Vec3 a, const Vec3 b) noexcept {
-    return {
-        a.y * b.z - a.z * b.y,
-        a.z * b.x - a.x * b.z,
-        a.x * b.y - a.y * b.x
-    };
 }
 
 EUCLID_QUALIFIER Vec4 EUCLID_CALL cross(const Vec4 a, const Vec4 b) noexcept {
